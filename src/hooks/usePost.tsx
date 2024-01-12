0fbfa079e51bd5bb2c48eadefec3705e0170fcc4
Guilherme Gualberto Souza <contatoguilhermegsz@gmail.com>
2024-01-12 13:49:55 -0300

    fix: request cors error

--- src/hooks/usePost.tsx
+++ src/hooks/usePost.tsx
@@ -1,10 +1,12 @@
-import { api } from "@/lib/api";
 import { IPost } from "@/types/post";
+import axios from "axios";
 import { useQuery } from "react-query";
 
 export const usePost = () => {
   const { data, isLoading } = useQuery<IPost[]>("posts", () =>
-    api.get("/posts?categories=373").then((response) => response.data),
+    axios
+      .get("https://www.olivas.digital/wp-json/wp/v2/posts?categories=373")
+      .then((response) => response.data),
   );
 
   const formatter = new Intl.DateTimeFormat("pt-br");
--- src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  headers: { "Access-Control-Allow-Origin": "*" },
-  baseURL: "https://www.olivas.digital/wp-json/wp/v2/",
-});
