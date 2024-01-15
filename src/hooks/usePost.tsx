435e2acfc8ef5562f24ba1264dbab340c8ec04d3
Guilherme Gualberto Souza <contatoguilhermegsz@gmail.com>
2024-01-14 21:09:01 -0300

    fix: not have errors

--- src/hooks/usePost.tsx
+++ src/hooks/usePost.tsx
@@ -1,3 +1,4 @@
+import { api } from "@/lib/api";
 import { IPost } from "@/types/post";
 import { useQuery } from "react-query";
 
@@ -5,10 +6,9 @@ export const usePost = (number: number) => {
   const { data, isLoading } = useQuery<IPost[]>({
     queryKey: "posts",
     queryFn: async () => {
-      return await fetch(
-        `https://www.olivas.digital/wp-json/wp/v2/posts?categories=${number}`,
-        { mode: "cors" },
-      ).then((response) => response.json());
+      return await api
+        .get(`/posts?categories=${number}`)
+        .then((response) => response.data);
     },
   });
 
